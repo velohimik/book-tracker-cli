2aa12d3c33fed7cb63e988df36bdea286bf257b2
Andriy Rybalka <andrey.rybalka.himik@gmail.com>
2025-07-31 17:28:32 +0300

    Add entities, gitignore file

---
+++ src/main/java/com/himik/entities/Book.java
@@ -0,0 +1,14 @@
+package com.himik.entities;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    String title;
+    String author;
+    String genre;
+    String notes;
+    LocalDate addedDate;
+    LocalDate finishedDate;
+    ReadingStatus status;
+}
---
+++ src/main/java/com/himik/entities/ReadingStatus.java
@@ -0,0 +1,5 @@
+package com.himik.entities;
+
+public enum ReadingStatus {
+    PLANNED, READING, COMPLETED, ABANDONED
+}
